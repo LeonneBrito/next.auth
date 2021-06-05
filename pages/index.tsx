5d6890b18888420022277819406f9ea57ec8f118
Leonne Brito <contato@leonnebrito.com.br>
2021-06-05 16:41:53 -0300

    :sparkles: feat: update authContext

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
-import { FormEvent, useContext, useState } from 'react'
-import { AuthContext } from '../contexts/AuthContext';
-import styles from '../styles/Home.module.css'
+import { GetServerSideProps } from "next";
+import { FormEvent, useContext, useState } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import styles from "../styles/Home.module.css";
+import { withSSRGuest } from "../utils/withSSRGuest";
 
 export default function Home() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const { signIn } = useContext(AuthContext);
 
@@ -13,26 +15,30 @@ export default function Home() {
     const data = {
       email,
       password,
-    }
+    };
     await signIn(data);
   }
   return (
     <form onSubmit={handleSubmit} className={styles.container}>
-      <input 
-        type="email" 
-        value={email}  
-        onChange={e => setEmail(e.target.value)} 
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
-      <input 
-        type="password" 
-        value={password}  
-        onChange={e => setPassword(e.target.value)} 
-      />      
-    <button
-      type="submit"
-    >
-      Entrar
-    </button>
-  </form>
-  )
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Entrar</button>
+    </form>
+  );
 }
+
+export const getServerSideProps = withSSRGuest(
+  async (ctx) => {
+    return {
+      props: {},
+    };
+  }
+);
